9662a6c26bd61ee883506b5d9e5425cc83528f1e
jinzr <zengrui.jin0@gmail.com>
2024-02-07 10:52:08 +0800

    Update train.py

--- egs/libriheavy2mix/train.py
+++ egs/libriheavy2mix/train.py
@@ -40,7 +40,7 @@ parser.add_argument(
 
 
 def neg_sisdr_loss_wrapper(est_targets, targets):
-    return singlesrc_neg_snr(est_targets[:, 0], targets[:, 0]).mean()
+    return singlesrc_neg_sisdr(est_targets[:, 0], targets[:, 0]).mean()
     # return singlesrc_neg_sisdr(est_targets[:, 0], targets[:, 0]).mean()
 
 

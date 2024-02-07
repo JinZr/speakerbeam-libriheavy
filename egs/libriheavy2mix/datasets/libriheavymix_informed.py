13799ade2dd394c7f147a9dd2b3b0ce13224291f
jinzr <zengrui.jin0@gmail.com>
2024-02-07 10:59:39 +0800

    Update libriheavymix_informed.py

--- egs/libriheavy2mix/datasets/libriheavymix_informed.py
+++ egs/libriheavy2mix/datasets/libriheavymix_informed.py
@@ -189,24 +189,18 @@ class LibriheavyMixInformed(Dataset):
                 self.mixed_wav_list[enroll_key], sr=self.sample_rate
             )
 
-            mixture = torch.from_numpy(
-                mixture[2 * self.sample_rate : self.sample_rate * self.segment]
-            )
-            source = torch.from_numpy(
-                source[2 * self.sample_rate : self.sample_rate * self.segment]
-            )
-            enroll = torch.from_numpy(
-                enroll[2 * self.sample_rate : self.sample_rate * self.segment_aux]
-            )
+            mixture = torch.from_numpy(mixture[: self.sample_rate * 12])
+            source = torch.from_numpy(source[: self.sample_rate * 12])
+            enroll = torch.from_numpy(enroll[: self.sample_rate * self.segment_aux])
 
             mixture = torch.nn.functional.pad(
                 mixture,
-                (0, self.segment * self.sample_rate - mixture.shape[0]),
+                (0, self.sample_rate * 12 - mixture.shape[0]),
                 value=0,
             )
             source = torch.nn.functional.pad(
                 source,
-                (0, self.segment * self.sample_rate - source.shape[0]),
+                (0, self.sample_rate * 12 - source.shape[0]),
                 value=0,
             )
             enroll = torch.nn.functional.pad(
